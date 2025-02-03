4c0f587af1f68b402d970715ff3b148b27811b26
lord-of-the-lost <ignatov.99@yandex.ru>
2025-02-03 21:55:41 +0300

    Initial Commit

---
+++ MemesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemesApp
+//
+//  Created by Николай Игнатов on 03.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
